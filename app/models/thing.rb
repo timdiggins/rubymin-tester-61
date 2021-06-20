1a00925059ff81d60d3187e5b22f304959d2fa0f
Tim Diggins <tim@red56.uk>
2021-06-20 06:55:45 +0100

    demonstrate deprecations

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,2 +1,18 @@
 class Thing < ApplicationRecord
+  # @deprecated
+  def some_method_yard_deprecated
+  end
+
+  def some_method_rails_deprecated
+    ActiveSupport::Deprecation.warn
+  end
+
+  def some_method_undeprecated
+  end
+
+  def other_method
+    some_method_yard_deprecated
+    some_method_rails_deprecated
+    some_method_undeprecated
+  end
 end
